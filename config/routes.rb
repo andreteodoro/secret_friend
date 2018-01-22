4825399141dcb82d3aaf60da2eaf8f1dc53fc2ff
André Luiz Teodoro <andre.teodoro@eldorado.org.br>
2018-01-22 16:56:28 -0200

    Creating controllers, models and mailers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaigns/show'
+
+  get 'campaigns/index'
+
+  get 'campaigns/create'
+
+  get 'campaigns/update'
+
+  get 'campaigns/destroy'
+
+  get 'campaigns/raffle'
+
+  get 'pages/home'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
