5797cb81cc02bcc0665e868ce10cb0227dce6804
André Luiz Teodoro <andre.teodoro@eldorado.org.br>
2018-01-22 20:25:28 -0200

    Campaign view

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,18 +1,22 @@
 class Campaign < ApplicationRecord
-  before_validation :set_member, on: :create
-  before_validation :set_status, on: :create
-
   belongs_to :user
   has_many :members, dependent: :destroy
+  before_validation :set_member, on: :create
+  before_validation :set_status, on: :create
   enum status: [:pending, :finished]
   validates :title, :description, :user, :status, presence: true
 
+  def count_opened
+    members.where(open: true).count
+  end
+
   private
+
   def set_status
     self.status = :pending
   end
 
   def set_member
-    self.members << Member.create(name: self.user.name, email: self.user.email)
+    members << Member.create(name: user.name, email: user.email)
   end
 end
